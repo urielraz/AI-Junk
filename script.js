fb03bdd3fb5e41ef5e437c200d2adf7251de5944
urielraz <1urielraz@gmail.com>
2023-07-24 15:26:51 +0300

    input and output img

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const selectedFile = document.getElementById("input").files[0];
+const input = document.querySelector("input")
+const output = document.querySelector("output")
+
+let imagesArray = []
+
+input.addEventListener("change", () => {
+    const files = input.files
+    for (let i = 0; i < files.length; i++) {
+        imagesArray.push(files[i])
+      }
+    displayImages()
+ })
+
+ function displayImages(){
+    let images = ""
+    imagesArray.forEach((image, index) => {
+      images += `<div class="image">
+                  <img src="${URL.createObjectURL(image)}" alt="image">
+                  <span onclick="deleteImage(${index})">&times;</span>
+                </div>`
+    })
+    output.innerHTML = images
+ }
