01dd4918a08627682310781d0158a1e87bdb5453
urielraz <1urielraz@gmail.com>
2023-07-25 15:51:30 +0300

    add test

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ input.addEventListener("change", () => {
                   <img src="${URL.createObjectURL(image)}" alt="image">
                   <span onclick="selectImage(${index})">&times;</span>
                 </div>`;
-               
+               console.log(index)
     })
     console.log(images);
     output.innerHTML = images
@@ -32,4 +32,19 @@ input.addEventListener("change", () => {
 function selectImage(index){
     const select = document.querySelector(`#image_${index}`)
     console.log(select)
+ }
+ 
+
+ function turnRight(){
+    console.log("da")
+    let src = cv.imread('bdika');
+    let dst = new cv.Mat();
+    let dsize = new cv.Size(src.rows, src.cols);
+    let center = new cv.Point(src.cols / 2, src.rows / 2);
+    // You can try more different parameters
+    let M = cv.getRotationMatrix2D(center, 45, 1);
+    cv.warpAffine(src, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+    cv.imshow('canvasOutput', dst);
+    src.delete(); dst.delete(); M.delete();
+
  }
