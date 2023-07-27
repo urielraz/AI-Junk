6c58a65aa970b6d9619a42230b2a2b8130eb97c8
urielraz <1urielraz@gmail.com>
2023-07-27 23:18:14 +0300

    canvas works but a litle grow

--- script.js
+++ script.js
@@ -12,12 +12,14 @@ input.addEventListener("change", () => {
     for (let i = 0; i < files.length; i++) {
         imagesArray.push(`img_`);
       //   console.log(imagesArray.i)
-        images += `<div  class="image">
-        <img onclick="selectImage(event)" id="img_${imagesArray.length-1}" src="${URL.createObjectURL(files[i])}" alt="image">
-      </div>`;
+        images += `
+        <img style="display: block;" onclick="selectImage(event)" id="img_${imagesArray.length-1}" src="${URL.createObjectURL(files[i])}" alt="image">
+        <canvas style="display: none;" id="canvas_img_${imagesArray.length-1}"></canvas>
+
+      `;
    };
    currentImage = `img_${imagesArray.length-1}`;
-   console.log(currentImage);
+   // console.log(currentImage);
    output.innerHTML += images
 
  })
@@ -49,12 +51,14 @@ function turnRight(){
    
    
 function rotateImage(parameter){
+   const canvas = document.getElementById(`canvas_${currentImage}`)
+
    const degrees = parameter + document.querySelector("#angleInput").value
    const angle =  parseFloat(degrees)
-   console.log(typeof(angle))
-   console.log(angle)
+   // console.log(typeof(angle))
+   // console.log(angle)
 
-   console.log(imagesArray[0])
+   // console.log(imagesArray[0])
    let src = cv.imread(currentImage);
    let dst = new cv.Mat();
    let dsize = new cv.Size(src.rows, src.cols);
@@ -62,7 +66,29 @@ function rotateImage(parameter){
    // You can try more different parameters
    let M = cv.getRotationMatrix2D(center, angle, 1);
    cv.warpAffine(src, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-   cv.imshow('canvasOutput', dst);
+   cv.imshow(canvas, dst);
    src.delete(); dst.delete(); M.delete();
+
+   canvas.style.display = "block";
+   document.getElementById(currentImage).style.display = "none";
+
+
+
+   const imageElement = document.getElementById(currentImage);
+   const parentDiv = imageElement.parentElement;
+
+   // const parentDiv = currentImage.parentElement.nodeName;
+   console.log(parentDiv);
+
+
+
+   // const canvasElement = document.createElement('canvas');
+   // canvasElement.id = currentImage.id;
+   // canvasElement.width = imageElement.width;
+   // canvasElement.height = imageElement.height;
+   // canvasElement.style.display = 'block';
+   // parentDiv.replaceChild(canvasElement, imageElement);
+
+   // currentImage.file = new Blob([cv.matFromImageData(dst).data], { type: 'image/jpeg' });
 
  }
