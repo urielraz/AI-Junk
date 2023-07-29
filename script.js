33b2da09b7cc6de33c5518ecadf08e8911bdd77b
urielraz <1urielraz@gmail.com>
2023-07-29 22:21:57 +0300

    before solve the last problem

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const input = document.querySelector("#fileInput")
 const output = document.querySelector("output")
 
 let currentImage = -1;
-
 const imagesArray = []
 
 function getRandomPosition() {
@@ -13,63 +12,50 @@ function getRandomPosition() {
    const randomLeft = Math.floor(Math.random() * (viewportWidth - 100));
  
    return { top: randomTop, left: randomLeft };
- }
- 
+}
 
+function addImageToOutput(file, index){
+
+   const randomPosition = getRandomPosition();
+
+   const divElement = document.createElement('div');
+   //   divElement.className = 'images';
+   divElement.style.position = 'absolute';
+   divElement.style.top = randomPosition.top + 'px';
+   divElement.style.left = randomPosition.left + 'px';
+   // divElement.style.height = '350px';
+   divElement.onclick = selectImage;
+   
+   const imgElement = document.createElement('img');
+   imgElement.id = index;
+   imgElement.src = URL.createObjectURL(file);
+   imgElement.style.height = '250px';
+   imgElement.alt = 'image';
+
+   const canvasElement = document.createElement('canvas');
+   canvasElement.style.display = 'none';
+   canvasElement.style.height = '250px';
+   canvasElement.id = 'canvas_' + (index);
+
+   divElement.appendChild(imgElement);
+   divElement.appendChild(canvasElement);
+
+   output.appendChild(divElement);
+} 
+ 
 input.addEventListener("change", () => {
     const files = input.files
-    console.log(output.innerHTML);
-    let images = ""
-    
     for (let i = 0; i < files.length; i++) {
-       const randomPosition = getRandomPosition();
-
-        imagesArray.push(`img_`);
-
-        const divElement = document.createElement('div');
-      //   divElement.className = 'images';
-        divElement.style.position = 'absolute';
-        divElement.style.top = randomPosition.top + 'px';
-        divElement.style.left = randomPosition.left + 'px';
-        divElement.onclick = selectImage;
-      
-        // Create the image element
-        const imgElement = document.createElement('img');
-        imgElement.id = imagesArray.length - 1;
-        imgElement.src = URL.createObjectURL(files[i]);
-        imgElement.alt = 'image';
-      
-        // Create the canvas element
-        const canvasElement = document.createElement('canvas');
-        canvasElement.style.display = 'none';
-      //   canvasElement.style.height = '150px';
-        canvasElement.id = 'canvas_' + (imagesArray.length - 1);
-      
-        // Append the image and canvas elements to the outer div
-        divElement.appendChild(imgElement);
-        divElement.appendChild(canvasElement);
-
-        output.appendChild(divElement);
-      //   images += `<div class="images" style="position: absolute; top: ${randomPosition.top}px; left: ${randomPosition.left}px;"  onclick="selectImage(event)">
-      //   <img  id="${imagesArray.length-1}" src="${URL.createObjectURL(files[i])}" alt="image">
-      //   <canvas style="display: none;" height:150px;"   id="canvas_${imagesArray.length-1}" ></canvas>
-      // </div>
-      // `;
+      imagesArray.push(`img_`);
+      addImageToOutput(files[i], imagesArray.length - 1)
    };
    currentImage = `${imagesArray.length-1}`;
-   // output.innerHTML += images
-   console.log(output)
-
  })
 
 function selectImage(event){
- 
    if (event.target.tagName === 'CANVAS') {
-
-      let parentDiv = event.target.parentNode;
-
-      let imgElement = parentDiv.querySelector('img');
-
+      const parentDiv = event.target.parentNode;
+      const imgElement = parentDiv.querySelector('img');
       currentImage = imgElement.id;
       
    } else if (event.target.tagName === 'IMG') {
@@ -77,14 +63,6 @@ function selectImage(event){
    }
 }
 
-function turnLeft(){
-   rotateImage("-")
-}
-
-function turnRight(){
-   rotateImage("+")
-}
-   
 function rotateImage(parameter){
    const canvas = document.getElementById(`canvas_${currentImage}`)
 
@@ -103,5 +81,13 @@ function rotateImage(parameter){
    document.getElementById(currentImage).style.display = "none";
    canvas.style.display = "block";
  
-
  }
+
+ function turnLeft(){
+   rotateImage("-")
+}
+
+function turnRight(){
+   rotateImage("+")
+}
+   
